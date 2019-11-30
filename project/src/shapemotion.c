dab13439bc6d6b1c1a658d3e707ada0de99820b8
Matthew Montoya <msmontoya2@miners.utep.edu>
2019-11-29 21:49:00 -0700

    merged project file

--- project/src/shapemotion.c
+++ project/src/shapemotion.c
@@ -8,13 +8,13 @@
  */
 
 #include <msp430.h>
-#include <libTimer.h>
 #include <lcdutils.h>
 #include <lcddraw.h>
 #include <p2switches.h>
 #include <shape.h>
 #include <abCircle.h>
 #include "buzzer.h"
+#include "libTimer.h"
 
 #define GREEN_LED BIT6
 
