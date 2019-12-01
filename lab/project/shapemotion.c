5a04cb9affcb85e3f733cb6643138354bec0c674
Matthew Montoya <msmontoya2@miners.utep.edu>
2019-11-30 22:37:49 -0700

    merged project file

--- lab/project/shapemotion.c
+++ lab/project/shapemotion.c
@@ -242,7 +242,9 @@ void main()
     }
     P1OUT |= GREEN_LED; /**< Green led on when CPU on */
     redrawScreen = 0;
-    movLayerDraw(&ml4, &lower_shark_layer);
+
+    // Draw Base Layer
+    movLayerDraw(&ml0, &human_head_layer);
   }
 }
 
