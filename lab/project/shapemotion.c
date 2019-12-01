3301cce825e9b28d53a5fb2afcc508f8957f05b6
Matthew Montoya <msmontoya2@miners.utep.edu>
2019-11-30 20:10:24 -0700

    merged project file

--- lab/project/shapemotion.c
+++ lab/project/shapemotion.c
@@ -101,10 +101,10 @@ Layer human_layer = {
 Layer layer0 = {
     // Innocent Seal
     (AbShape *)&rectangle_size_7,
-    {screenWidth / 2, screenHeight / 2 + 5}, /**< center */
+    {screenWidth / 2+10, screenHeight / 2 + 7}, /**< center */
     {0, 0},
     {0, 0}, /* last & next pos */
-    COLOR_WHITE,
+    COLOR_RED,
     &human_layer,
 };
 
