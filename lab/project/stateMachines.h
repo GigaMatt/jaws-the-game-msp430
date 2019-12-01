aaf07d796ebb241d9e8e790dac2adf90c0dc77e8
Matthew Montoya <msmontoya2@miners.utep.edu>
2019-12-01 00:31:21 -0700

    Experimental

--- lab/project/stateMachines.h
+++ lab/project/stateMachines.h
@@ -4,6 +4,9 @@
 #define stateMachine_included
 
 void state_advance();
+void sm_fast_clock();
+void sm_slow_clock();
+void sm_update_led();
 char toggle_red();
 char toggle_green();
 char toggle_red_green();
