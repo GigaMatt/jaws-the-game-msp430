ce093902a17c094854e6efacde87c3c8d1ccdbe8
Matthew Montoya <msmontoya2@miners.utep.edu>
2019-11-30 20:12:58 -0700

    merged project file

--- lab/project/shapemotion.c
+++ lab/project/shapemotion.c
@@ -91,7 +91,7 @@ Layer border_field_layer = {
 Layer human_layer = {
     // Swimming Human
     (AbShape *)&rectangle_size_10,                 // Human as
-    {screenWidth / 2 + 40, screenHeight / 2 + 51}, /**< center */
+    {screenWidth / 2 + 40, screenHeight / 2}, // center  + 51
     {0, 0},
     {0, 0}, /* last & next pos */
     COLOR_BROWN,
