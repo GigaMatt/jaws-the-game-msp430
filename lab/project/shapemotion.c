8a2ec609f8c3946c2627f9a8c841c8abe168a83e
Matthew Montoya <msmontoya2@miners.utep.edu>
2019-11-30 22:48:10 -0700

    merged project file

--- lab/project/shapemotion.c
+++ lab/project/shapemotion.c
@@ -196,7 +196,7 @@ void mlAdvance(MovLayer *ml, Region *fence)
         }
         if (velocity > 0)
         {
-          drawString5x7(35, 95, "CIRCA 1975 ", COLOR_RED, COLOR_BLACK); //prev 5x7 20, 35          
+          drawString5x7(35, 100, "CIRCA 1975 ", COLOR_RED, COLOR_BLACK); //prev 5x7 20, 35          
         }
       } /**< if outside of fence */
     }   /**< for axis */
