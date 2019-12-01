e2137aa1d5b6679d91d4da4f920e749adb26382b
Matthew Montoya <msmontoya2@miners.utep.edu>
2019-11-30 21:17:52 -0700

    merged project file

--- lab/project/shapemotion.c
+++ lab/project/shapemotion.c
@@ -61,7 +61,7 @@ Layer layer4 = {
     // Upper Shark
 
     (AbShape *)&right_arrow,
-    {(screenWidth / 2)-15, (screenHeight / 2)-47}, /**< bit below & right of center +5,+5*/
+    {(screenWidth / 2)+15, (screenHeight / 2)-47}, /**< bit below & right of center +5,+5*/
     {0, 0},
     {0, 0}, /* last & next pos */
     COLOR_WHITE,
